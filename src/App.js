569bb7d5a989ef1b0e280c4e2c0fe70f4c182256
Cesar <camorais@gmail.com>
2020-03-08 06:37:26 +0000

    login-firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,93 @@
-import React from "react";
+import React, { Component } from 'react';
 import './index.css';
 import './App.css';
+import firebase from "firebase";
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
 
 import Navbar from "./components/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    // when loggedInUser is set from FireBase Auth,
+    // it will be an object with lots of keys, two useful ones
+    // we can use are: loggedInUser.uid and loggedInUser.email :)
+    this.state = { loggedInUser: null };
+  }
+
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAhTN1_TEcmNT3xhnId2U6mISUrqSXwEpg",
+      authDomain: "ironplaces-auth.firebaseapp.com",
+      databaseURL: "https://ironplaces-auth.firebaseio.com",
+      projectId: "ironplaces-auth",
+      storageBucket: "ironplaces-auth.appspot.com",
+      messagingSenderId: "420598064107",
+      appId: "1:420598064107:web:745862904ecfcf002c6fae",
+    });
+  }
+
+  saveUserToServer = (name, isAdmin, uId) => {
+
+  }
+
+  createNewFbaseUser = (email, password, callbackNavToProj) => {
+    // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#createuserwithemailandpassword
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(resp => {
+      // console.log(resp);
+      this.setState({loggedInUser: resp.user});
+      // saveUserToServer();
+      callbackNavToProj();
+    })
+    .catch(err => alert(err));
+  }
+
+  loginFbaseUser = (email, password, callbackNavToProj) => {
+    // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signinwithemailandpassword
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(resp => {
+      // console.log(resp);
+      this.setState({loggedInUser: resp.user});
+      // this.props.history.push('/projects'); --> this will cause error
+      // we do not have access to this.props.history here!
+      // so we can use a calllback function instead :)
+      callbackNavToProj();
+    })
+    .catch(err => alert(err));
+  }
+
+  logoutFbaseUser = () => {
+    // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#sign-out
+    firebase.auth().signOut()
+    .then(resp => {
+      console.log("User has been logged out", resp);
+      this.setState({loggedInUser: null});
+    })
+    .catch(err => alert(err));
+  }
+  
+  render() {
+    return (
     <div className="App">
       <BrowserRouter>
         <div className="main-container">
-          <Navbar />
+        <Navbar loggedInUser={this.state.loggedInUser} logoutFbase={this.logoutFbaseUser}/>
+          <Switch>
+          <Route exact path='/signup' render={(props) => <Signup createNewFbaseUser={this.createNewFbaseUser} {...props} />} />
+          <Route exact path='/login'  render={(props) => <Login loginFbaseUser={this.loginFbaseUser} {...props} />} />
+          {/* <Route exact path="/projects" component={ProjectList}/>
+          <Route exact path="/projects/:id" component={ProjectDetails} />
+          <Route exact path="/projects/:id/tasks/:taskId" component={TaskDetails} /> */}
+        </Switch>
         </div>
       </BrowserRouter>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,67 @@
 import React, {Component} from 'react';
-//import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
 
 class Navbar extends Component {
+  renderAuthLinks() {
+    // console.log("navbar: props.loggedInUser -> ", this.props.loggedInUser);
+    const {loggedInUser, logoutFbase} = this.props;
+    if (!loggedInUser) {
+      return (
+        // same as using <React.Fragment>
+        <>
+          <li><Link to="/signup">Sign Up</Link></li>
+          <li><Link to="/login">Log In</Link></li>
+        </>
+        // same as using </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <li>Welcome, {loggedInUser.email}</li>
+          <li><Link to="/" onClick={logoutFbase}>Log Out</Link></li>
+        </React.Fragment>
+      );
+    }
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <div className="navbar">
-          <img src={logo} alt="logo"/>
-          <h1>Ironplaces</h1>
-          <div className="dropdown">
-            <button className="btn btn-sec dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Login
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a className="dropdown-item" href="/login/admin">Admin</a>
-              <a className="dropdown-item" href="/login/student">Student</a>
-            </div>
-          </div>
-        </div>
-      </React.Fragment>
-      
-    )
+      <nav className="nav-style">
+        <ul>
+          {this.renderAuthLinks()}
+          {/* <li><Link to="/projects">Projects</Link></li> */}
+      </ul>
+      </nav>
+    );
   }
 }
+
+
+
+
+
+// class Navbar extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <div className="navbar">
+//           <img src={logo} alt="logo"/>
+//           <h1>Ironplaces</h1>
+//           <div className="dropdown">
+//             <button className="btn btn-sec dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//               Login
+//             </button>
+//             <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+//               <a className="dropdown-item" href="/login/admin">Admin</a>
+//               <a className="dropdown-item" href="/login/student">Student</a>
+//             </div>
+//           </div>
+//         </div>
+//       </React.Fragment>
+      
+//     )
+//   }
+// }
 
 export default Navbar;
